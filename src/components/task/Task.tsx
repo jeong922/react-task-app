f58329067ae2652280bdf0c15e652b5e28bc630f
jeong922 <hwajeong922@naver.com>
2024-05-16 17:31:09 +0900

    task 추가 및 스타일링

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { FC } from 'react';
+import { container, description, title } from './Task.css';
 
-const Task = () => {
-  return <div>Task</div>;
+type TaskProps = {
+  index: number;
+  id: string;
+  boardId: string;
+  taskName: string;
+  taskDescription: string;
+};
+
+const Task: FC<TaskProps> = ({
+  taskName,
+  taskDescription,
+  boardId,
+  id,
+  index,
+}) => {
+  return (
+    <div className={container}>
+      <div className={title}>{taskName}</div>
+      <div className={description}>{taskDescription}</div>
+    </div>
+  );
 };
 
 export default Task;
