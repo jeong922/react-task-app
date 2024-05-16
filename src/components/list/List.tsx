888a1641c714e2e0424534475d7fa69c654c1751
jeong922 <hwajeong922@naver.com>
2024-05-16 16:39:54 +0900

    listContainer 추가

--- src/components/list/List.tsx
+++ src/components/list/List.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-const List = () => {
+const List = ({ list, boardId }) => {
   return <div>List</div>;
 };
 
--- src/components/listsContainer/ListsContainer.tsx
+++ src/components/listsContainer/ListsContainer.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import { FC } from 'react';
+import { List as listType } from '../../types';
+import List from '../list/List';
+import ActionButton from '../actionButton/ActionButton';
 
-const ListsContainer = () => {
-  return <div>ListsContainer</div>;
+type ListContainerProps = {
+  boardId: string;
+  lists: listType[];
+};
+
+const ListsContainer: FC<ListContainerProps> = ({ lists, boardId }) => {
+  return (
+    <div>
+      {lists.map((list) => (
+        <List key={list.listId} list={list} boardId={boardId} />
+      ))}
+      <ActionButton />
+    </div>
+  );
 };
 
 export default ListsContainer;
