5737e146b66efa99f0ab1ceb460a9899bad9543e
jeong922 <hwajeong922@naver.com>
2024-05-17 15:13:03 +0900

    logItem 추가

--- src/components/loggerModal/logItem/LogItem.tsx
+++ src/components/loggerModal/logItem/LogItem.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import { LogItem as LogItemType } from '../../../types';
+import { BsFillPersonFill } from 'react-icons/bs';
 
-export const LogItem = () => {
-  return <div>LogItem</div>;
+type LogItemProps = {
+  logItem: LogItemType;
+};
+
+export const LogItem = ({ logItem }: LogItemProps) => {
+  const timeOffset = new Date(Date.now() - Number(logItem.logTimestamp));
+
+  const showOffsetTime = `${
+    timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ''
+  } ${timeOffset.getSeconds() > 0 ? `${timeOffset.getMinutes()}s ago` : ''} ${
+    timeOffset.getSeconds() === 0 ? `just now` : ''
+  }`;
+
+  return (
+    <div>
+      <div>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div>{logItem.logMessage}</div>
+      <div>{showOffsetTime}</div>
+    </div>
+  );
 };
