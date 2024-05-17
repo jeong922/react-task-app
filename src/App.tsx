2407e2adf755e340395ea813e3489ea1ef39f550
jeong922 <hwajeong922@naver.com>
2024-05-17 14:33:42 +0900

    loggerModal 추가

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,10 @@ import BoardList from './components/boardList/BoardList';
 import ListsContainer from './components/listsContainer/ListsContainer';
 import { useTypedSelector } from './hooks/redux';
 import EditModal from './components/editModal/EditModal';
+import { LoggerModal } from './components/loggerModal/LoggerModal';
 
 function App() {
+  const [isLoggerOpen, setIsLoggerOpen] = useState(false);
   const [activeBoardId, setActiveBoardId] = useState('board-0');
   const modalActive = useTypedSelector((state) => state.boards.modalActive);
   const boards = useTypedSelector((state) => state.boards.boardArray);
@@ -18,6 +20,7 @@ function App() {
 
   return (
     <div className={appContainer}>
+      {isLoggerOpen ? <LoggerModal setIsLoggerOpen={setIsLoggerOpen} /> : null}
       {modalActive ? <EditModal /> : null}
       <BoardList
         activeBoardId={activeBoardId}
@@ -28,7 +31,9 @@ function App() {
       </div>
       <div className={buttons}>
         <button>이 게시판 삭제하기</button>
-        <button></button>
+        <button onClick={() => setIsLoggerOpen(!isLoggerOpen)}>
+          {isLoggerOpen ? '활동 목록 숨기기' : '활동 목록 보이기'}
+        </button>
       </div>
     </div>
   );
--- src/components/loggerModal/LoggerModal.tsx
+++ src/components/loggerModal/LoggerModal.tsx
@@ -1,5 +1,26 @@
-import React from 'react';
+import { FiX } from 'react-icons/fi';
+import { useTypedSelector } from '../../hooks/redux';
+import { LogItem } from './logItem/LogItem';
 
-export const LoggerModal = () => {
-  return <div>LoggerModal</div>;
+type LoggerModalProps = {
+  setIsLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const LoggerModal = ({ setIsLoggerOpen }: LoggerModalProps) => {
+  const logs = useTypedSelector((state) => state.logger.logArray);
+  return (
+    <div>
+      <div>
+        <div>
+          <div>활동 기록</div>
+          <FiX onClick={() => setIsLoggerOpen(false)} />
+        </div>
+        <div>
+          {logs.map((log) => (
+            <LogItem key={log.logId} logItem={log} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
